40d82359fb4c634442c70eb00d5d0d6950afcae0
Michael Soares <mikey.soares@gmail.com>
2019-01-29 19:52:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,15 +8,13 @@ class Triangle
     if @sides.any? {|side| side <= 0}
       raise TriangleError.new, "Cannot create a triangle "
     end
-
-    @sides.combonation(2).each do |combo|
+    @sides.combonation(2).each do |combonation|
       last_side = @sides.clone
-      combo.each {|item| last_side.delete(item)}
+      combonation.each {|item| last_side.delete(item)}
       if !last_side.empty? && combo.inject(:+) <= last_side.first
         raise TriangleError.new, "Cannot create a triangle"
       end
     end
-
     classify
   end
 
