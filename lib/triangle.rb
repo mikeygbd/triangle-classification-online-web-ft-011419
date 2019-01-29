1f21e3d3d572410fe569edbf2e6bf95911728598
Michael Soares <mikey.soares@gmail.com>
2019-01-29 19:33:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,9 +10,21 @@ class Triangle
 
   end
 
-  def kind
-    if self.same_values?
-      @equilateral
+  def kind(a, b, c)
+    triangles = {equilateral: [60,60,60], isosceles: [0], scalene: [0]}
+    angle_a = (Math.acos((two**2+three**2-one**2)/(2*two*three).to_f)*180/Math::PI).round(2)
+    angle_b = (Math.acos((three**2+one**2-two**2)/(2*three*one).to_f)*180/Math::PI).round(2)
+    angle_c = (Math.acos((one**2+two**2-three**2)/(2*one*two).to_f)*180/Math::PI).round(2)
+    if angle_a && angle_b == 60
+      return :equilateral, triangles[:equilateral]
+    elsif angle_a != angle_b && angle_b != angle_c
+      triangles[:scalene] = [angle_a, angle_b, angle_c]
+      return :scalene, triangles[:scalene]
+    else
+      triangles[:isosceles] = [angle_a, angle_b, angle_c]
+      return :isosceles, triangles[:isosceles]
+    end
+  end
 
 
     # if @sides.same_values?
