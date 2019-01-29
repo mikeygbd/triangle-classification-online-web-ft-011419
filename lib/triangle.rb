5d9dd8464545c40349a8345d3c3018bf347dfdf2
Michael Soares <mikey.soares@gmail.com>
2019-01-29 19:21:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3, :equilateral, :isosceles, :scalene
+
+
+  def initialize(side1, side2, side3)
+
+
+
+
+  end
+
+  def kind
+
+  end
 end
